65a2d4c80f500b9cfc0c279b85f8db702e6dbad3
harshadaajabale <harshadaajabale12@gmail.com>
2023-05-11 08:47:48 -0900

    create function and varibale.js
    to create function and variable.js

---
+++ 01_variable-assign.js
@@ -0,0 +1,15 @@
+
+
+var myFullName = "Harshada Ajabale";
+console.log(myFullName);
+
+var myFullName; //Variable Declaration
+myFullName = "Harshada Anantrao Ajabale"  // updated
+console.log(myFullName);
+
+var myLove = "I love only JS";  
+console.log(myLove);
+
+var info;
+info = "Harshada Ajabale 22 Prabhat Colony Shilangan road, Amravati 444601 Female No";
+console.log(info);

5939fc6cf6cf42e474b52dddab7f23abf514ed15
harshadaajabale <harshadaajabale12@gmail.com>
2023-05-11 15:32:21 -0900

    To create 02_swapvariable.je
    to create 02_swapVariable.js

---
+++ 02_swapVariableAssign.js
@@ -0,0 +1,21 @@
+var sweety = "Sweety";
+var cutie = "Cutie"; 
+
+console.log("Before Swap..", sweety,  cutie);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("After Swap..",  sweety, cutie);
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+// Output --> num1 = 200,  num2 =300, num3 = 100
+
+console.log("Before Swap..", "num1: ", num1, " num2: ", num2, "num3", num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("After Swap..", "num1:", num1, " num2:", num2, "num3:", num3);

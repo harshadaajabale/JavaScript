507d7033f2a7b478ba022db7a34645f74df6a4ce
harshadaajabale <harshadaajabale12@gmail.com>
2023-05-20 21:52:26 -0900

    create 08_foraLoop.js
    create 08_foraLoop.js

---
+++ 08_foraLoop.js
@@ -0,0 +1,43 @@
+// WAP to reverse the string --> JavaScript --> tpircSavaJ
+// JavaScript -->
+// initialization = 9
+// condition index>=0
+// update expression index--
+var str = "JavaScript";
+var reverseString = "";
+for (let index = 9; index >= 0; index--) { // index = 9  8 
+    var char = str.charAt(index);
+    reverseString = reverseString.concat(char); 
+}
+console.log(reverseString);
+
+
+function reverseWord(word){
+    var reverseString = "";
+    var wordLength = word.length - 1;
+    for (let index = wordLength; index >= 0; index--) { // index = 9  8 
+        var char = word.charAt(index);
+        reverseString = reverseString.concat(char);    
+    }
+console.log(reverseString);
+}
+reverseWord("JavaScript");
+reverseWord("Google Chrome");
+reverseWord("Developer");
+
+
+
+var word = "JavaScript Language Of Internet";
+// a e i o u == A E I O U
+var count = 0;
+for (let index = 0; index < word.length; index++) {
+    var char = word.charAt(index); // 
+    if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || 
+        char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U') {
+        console.log(char);
+        count++;
+    }
+    
+}
+console.log(`Count total vowels: ${count}`);
+

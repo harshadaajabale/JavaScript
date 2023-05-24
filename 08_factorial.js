a52128dbf65e98310f45a862cca513d18438e97f
harshadaajabale <harshadaajabale12@gmail.com>
2023-05-24 08:15:20 -0900

    create08_ factorial.js
    create08_ factirial.js

---
+++ 08_factorial.js
@@ -0,0 +1,33 @@
+//5! = 5*4*3*2*1
+let factorial = 1;    //120
+for (let index = 5; index >= 1; index--) { // index = 5 4 3 2 1 0 
+    
+    factorial = factorial * index; // 120 *1
+}
+console.log(`Factorial ${factorial}`);
+
+
+
+
+console.log(" zero factorial");
+
+function factorialNum(num){
+
+
+
+    if (num==0) {
+        console.log(`factorial of zero is 1`);
+        return;
+        
+    }
+    let factorial = 1;
+    for (let index = num; index >= 1; index--) {
+        factorial = factorial * index;
+        
+    }
+    console.log(`factorial ${factorial}`);
+}
+factorialNum(5);
+factorialNum(6);
+factorialNum(10);
+factorialNum(0);

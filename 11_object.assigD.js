4ad59a5fa43be40002ad9ebc6b51cb855818ed31
harshadaajabale <harshadaajabale12@gmail.com>
2023-06-01 21:42:00 -0900

    create object_assignA.js
    create object_assignA.js

--- 11_object.assigD.js
+++ 11_object.assigD.js
@@ -1,93 +1,61 @@
+console.log("Harshada A. Ajabale");
 
-let degree = {
-    BSC: "CS",
-    name: "Harshada Ajabale",
-    graduationMarks: 73,
-    branch: "Computer Science",
-    age: 24,
-    gender: "female",
-    
-    engineering: {
-     branch: "CSC",
-     name: "Shreyash",
-     graduationMarks: 80,
-     branch: "BE",
-     age: 28,
-     gender: "male",
-    
-    
-    phd : {
-    branch: "ADv computing",
-    name: "Anvi",
-    graduationMarks: 90,
-    age: 26,
-    gender: "female",
-
-    }
-    // certificates : ["hacker Rank Participation", "IFE course", "ADv Programming"],
-    // certificates = person.certificates[1],
-
-
-    },
+let professor={
+  name:"Harshada",
+  age:"25",
+  gender:"female",
+  subject:"computer science",
+  college:"hvpm",
+  degree : {
+      engineering:"CSE",
+      PHD:"adv Computing",
+      diploma:"cyber security"
+  },
+  certificate:["Hacker Rank Participation", "IFE course", "Adv Programming"],
+  value :function(){
+      let degrees=`engineering: ${this.degree.engineering},
+                      PHD: ${this.degree.PHD}, Diploma : ${this.degree.diploma}`;
+      return degrees;
+  }
+};
+console.log(`Type of  professor is ${typeof  professor}`);
+console.log(`----------------------step 1. -------------------------------------`);
+console.log(`1.Name: ${professor.name}`);
+console.log(`2.age: ${professor.age}`);
+console.log(`3.gender: ${professor.gender}`);
+console.log(`4.college: ${professor.college}`);
+console.log(`5.subject: ${professor.subject}`);
 
 
 
-    //    certificates : ["hacker Rank Participation", "IFE course", "ADv Programming"],
-    //     certificates = person.certificates[1],
-    // //    console.log(element),
-    // //    for ( index = 0, index < certificates.length, index++) {
-    // //     const element = certificates[index],
-    // //     console.log(element),
-
-        
-        
-       };
-
-    console.log(`Type of degree is: ${typeof degree}`);
-
-    console.log(`Person name is : ${degree.engineering.name}`);
-    console.log(`Person branch is : ${degree.engineering.branch}`);
-    console.log(`Person age is : ${degree.engineering.age}`);
-    console.log(`Person graduationMarks is : ${degree.engineering.graduationMarks}`);
-  
-  console.log(`Person gender is : ${degree.engineering.gender}`);
+console.log(`--------------------step 2..------------------`);
+console.log(professor.degree);
 
 
+console.log(`----------------------step 3.--------------------`);
+console.log(professor.certificate);
 
 
-  console.log(`Type of degree is: ${typeof degree}`);
-console.log(`Person name is : ${degree.phd.name}`);
-  
-    console.log(`Person branch is : ${degree.phd.branch}`);
-    console.log(`Person age is : ${degree.phd.age}`);
-    console.log(`Person graduationMarks is : ${degree.phd.graduationMarks}`);
-  
-  console.log(`Person gender is : ${degree.phd.gender}`);
+console.log(`----------------------step 4.---------------------- `);
+let show=professor.value();
+console.log(`total degrees are ==>${show}`);
 
 
-  
-  
-       
+console.log(`---------------------------step 5. ----------------------------`);
+professor.totalExperience="14 years";
+console.log(`total Experience is : ${professor.totalExperience}`);
 
 
+console.log(`--------------------------step 6.------------------------------`);
+professor.age=35;
+console.log(professor);
 
 
+console.log(`----------------------------step 7.------------------`);
+professor.certificate.push("Oracle Cerificate");
+console.log(professor.certificate);
 
 
-
-
-    
-    
-    
-
-// console.log(`Type of BSC is: ${BSC}`);
-// console.log(`Type of enginerring is : ${engineering}`);
-// console.log(`type of PHD is: ${PHD}`);
-
-
-
-
-
-
-
-
+console.log(`-------------------step 8.--------------------------`);
+let n=professor.certificate.length;
+console.log(professor.certificate[n-1]);
---
+++ 11_object_assigE.js
@@ -0,0 +1,104 @@
+
+
+let bank = {
+    bankname: "SBI",
+    location: "Yavatmal, Maharashtra",
+    accountNo: 43435678920 ,
+    ifsc: `sbi34236` ,
+    interestRate: 300.10,
+
+    
+
+    
+}
+console.log(`Type of bank is ${typeof bank}`);
+console.log("============================================================");
+
+const sbiBankName = bank.bankname;
+const sbiBankLocation = bank.location;
+const sbiBankAccountNo = bank.accountNo;
+const sbiBankIfsc = bank.ifsc;
+const sbiBankInterestRate = bank.interestRate;
+
+console.log(`Bank name is : ${sbiBankName}`);
+console.log(`Bank location is : ${sbiBankLocation}`);
+console.log(`Bank accountNo is : ${sbiBankAccountNo}`);
+console.log(`Bank ifsc is : ${sbiBankIfsc}`);
+console.log(`Bank interestRate is : ${sbiBankInterestRate}`);
+
+console.log("=============================================================");
+bank.bank1= {
+        bankname: "Axis Bank ",
+        location: "Mumbai",
+        accountNo: 12345678910,
+        ifsc: `axis3236` ,
+        interestRate: 43.6000,
+    }
+const axisBankName = bank.bank1.bankname;
+const axisBankLocation = bank.bank1.location;
+const axisBankAccountNo = bank.bank1.accountNo;
+const axisBankIfsc = bank.bank1.ifsc;
+const axisBankInterestRate = bank.bank1.interestRate;
+
+
+console.log(`Bank name is : ${axisBankName}`);
+console.log(`Bank location is : ${axisBankLocation}`);
+console.log(`Bank accountNo is : ${axisBankAccountNo}`);
+console.log(`Bank ifsc is : ${axisBankIfsc}`);
+console.log(`Bank interestRate is : ${axisBankInterestRate}`);
+
+console.log("=====================================================================");
+
+bank.bank2= {
+    bankname: "hdfc ",
+    location: "Pune",
+    accountNo: 33344455576,
+    ifsc: `hdfc3236` ,
+    interestRate: 40.6000,
+}
+const hdfcBankName = bank.bank2.bankname;
+const hdfcBankLocation = bank.bank2.location;
+const hdfcBankAccountNo = bank.bank2.accountNo;
+const hdfcBankIfsc = bank.bank2.ifsc;
+const hdfcBankInterestRate = bank.bank2.interestRate;
+
+
+console.log(`Bank name is : ${hdfcBankName}`);
+console.log(`Bank location is : ${hdfcBankLocation}`);
+console.log(`Bank accountNo is : ${hdfcBankAccountNo}`);
+console.log(`Bank ifsc is : ${hdfcBankIfsc}`);
+console.log(`Bank interestRate is : ${hdfcBankInterestRate}`);
+
+
+
+console.log("================================================");
+bank.bank3= {
+    bankname: "yesBank ",
+    location: "Akola",
+    accountNo: 12345678901,
+    ifsc: `yes33236` ,
+    interestRate: 70.600,
+}
+const yesBankName = bank.bank3.bankname;
+const yesBankLocation = bank.bank3.location;
+const yesBankAccountNo = bank.bank3.accountNo;
+const yesBankIfsc = bank.bank3.ifsc;
+const yesBankInterestRate = bank.bank3.interestRate;
+
+
+console.log(`Bank name is : ${yesBankName}`);
+console.log(`Bank location is : ${yesBankLocation}`);
+console.log(`Bank accountNo is : ${yesBankAccountNo}`);
+console.log(`Bank ifsc is : ${yesBankIfsc}`);
+console.log(`Bank interestRate is : ${yesBankInterestRate}`);
+
+
+
+
+
+
+
+// const personName = person.name;
+//   const personHeight = person["height"];
+//   console.log(`Person name is : ${personName}`);
+//   console.log(`Person City is : ${person.city}`);

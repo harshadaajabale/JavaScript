155b22741cf2f0e8b2fa4b89cda871dc652c9907
harshadaajabale <harshadaajabale12@gmail.com>
2023-06-06 12:57:12 -0900

    08_foraLopAssign.js

---
+++ 17_forEachAssignA.js
@@ -0,0 +1,14 @@
+
+
+
+let arrayNum = [1, -7, 14, 40, 502, -77, 91, 0, 108, 89, -601];
+console.log(` Given ArrayNumbers ${arrayNum}`);
+
+
+console.log("step 1. ");
+arrayNum.forEach( ( index, array ) => {
+    console.log(` ${index}`);
+    console.log(array);
+}  );
+
+
